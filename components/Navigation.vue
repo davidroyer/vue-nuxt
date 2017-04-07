10acf988164e7eb92bd10aae5597f7109c10f729
David Royer <droyer01@gmail.com>
2017-04-07 13:12:07 -0400

    Initial commit

---
+++ components/Navigation.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <nav>
+    <li v-for="(link, key) in links">
+      <nuxt-link exact :to="link.href">{{link.title}}</nuxt-link>
+    </li>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [{
+        href: '/',
+        title: 'Home'
+      }, {
+        href: '/about',
+        title: 'About'
+      }, {
+        href: '/contact',
+        title: 'Contact'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+nav {
+    display: flex;
+    justify-content: center;
+    background: #35495e;
+    li {
+        list-style-type: none;
+        margin: 1em;
+
+        a {
+          color: white;
+          font-weight: 600;
+
+          &.nuxt-link-active {
+            color: gray;
+          }
+        }
+    }
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+  
+      <h1>The Contact Page</h1>
+
+
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
